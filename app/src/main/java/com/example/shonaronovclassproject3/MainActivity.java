1d1939334a6fd7a104f63b3b77a10a2c99457897
Magshimim_AI <shon40501@gmail.com>
2024-01-02 15:14:01 +0200

    First Commit

---
+++ app/src/main/java/com/example/shonaronovclassproject3/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.shonaronovclassproject3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button bt;
+    int counter = 0 ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt = (Button) findViewById(R.id.bt1);
+        bt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                counter += 1;
+                if(counter < 7){
+                    bt.setText("This is a click number: " + counter);
+                }
+                else {
+                    counter = 0;
+                    bt.setText("Enough to click. Go to new start!");
+                }
+            }
+        });
+    }
+}
